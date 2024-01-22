ae444e9876ad5fbc6991909a064650c24c760364
jacky <503354703@qq.com>
2024-01-22 15:48:54 +0800

    bak

---
+++ key.py
@@ -0,0 +1,24 @@
+from app.gui.windows import Windows
+from app.models.rsa import RsaModel
+from app.models.key import Key
+from app.models.api import ApiModel
+from app.models.mac import MacAddressModel
+import global_vars
+import multiprocessing
+import time
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    rsaModel = RsaModel()
+    publicKeyPath = global_vars.root_path + '/public_key.pem'
+    publicKey = rsaModel.publicKey(publicKeyPath)
+    message = "self1=7=" + time.strftime("%Y-%m-%d",time.gmtime())
+
+
+    data = rsaModel.encrytedData(message, publicKey)
+    print(data)
+    
+    
+
+
+
+
